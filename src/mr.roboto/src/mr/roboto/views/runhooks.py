77804599b1644ddc0d8a0ed0d631d933086b32f7
Gil Forcada <gforcada@gnome.org>
2016-04-17 13:22:22 +0200

    Add a new hook: handle pull requests

--- src/mr.roboto/src/mr/roboto/views/runhooks.py
+++ src/mr.roboto/src/mr/roboto/views/runhooks.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from collections import namedtuple
 from cornice import Service
 from github.GithubException import GithubException
 from mr.roboto.security import validate_service_token
@@ -22,10 +23,25 @@ createGithubPostCommitHooks = Service(
 def create_github_post_commit_hooks_view(request):
     """Re-create hooks on github
 
-    Removes all existing hooks on all repositories on github plone organization
-    and creates them anew.
+    Removes all existing hooks on all repositories on github plone and
+    collective organizations and creates them anew.
     """
     github = request.registry.settings['github']
+    roboto_url = request.registry.settings['roboto_url']
+
+    # hooks URL
+    Hook = namedtuple('Hook', ['url', 'events', ])
+    roboto_hooks = [
+        Hook(
+            '{0}/run/corecommit'.format(roboto_url),
+            ['push', ]
+        ),
+        Hook(
+            '{0}/run/pull-request'.format(roboto_url),
+            ['pullrequest', ]
+        ),
+    ]
+
     messages = []
 
     collective_repos = [
@@ -37,22 +53,19 @@ def create_github_post_commit_hooks_view(request):
     for repo_name in collective_repos:
         repo = collective.get_repo(repo_name.strip())
         messages.append(
-            update_hooks_on_repo(repo, request)
+            update_hooks_on_repo(repo, roboto_hooks, request)
         )
 
     for repo in github.get_organization('plone').get_repos():
         messages.append(
-            update_hooks_on_repo(repo, request)
+            update_hooks_on_repo(repo, roboto_hooks, request)
         )
 
     return json.dumps(messages)
 
 
-def update_hooks_on_repo(repo=None, request=None):
+def update_hooks_on_repo(repo=None, new_hooks=None, request=None):
     debug = request.registry.settings['debug']
-    # hooks URL
-    roboto_url = request.registry.settings['roboto_url']
-    commit_url = '{0}/run/corecommit'.format(roboto_url)
 
     # Remove old hooks
     hooks = repo.get_hooks()
@@ -68,17 +81,18 @@ def update_hooks_on_repo(repo=None, request=None):
                 hook.delete()
 
     # Add new hooks
-    msg = 'github Creating hook {0} on {1}'.format(commit_url, repo.name)
+    msg = 'github Creating hooks on {0}'.format(repo.name)
     logger.info(msg)
     try:
         if debug:
-            logger.info('Debug creating hook')
+            logger.info('Debug creating hooks')
         else:
-            config = {
-                'url': commit_url,
-                'secret': request.registry.settings['api_key']
-            }
-            repo.create_hook('web', config, ['push', ], True)
+            for hook in new_hooks:
+                config = {
+                    'url': hook.url,
+                    'secret': request.registry.settings['api_key']
+                }
+                repo.create_hook('web', config, hook.events, True)
     except GithubException:
         logger.exception('Error creating hook on {0}'.format(repo.name))
 
