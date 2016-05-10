576770ff1e06ed933334f8513233e05c4520eeec
Gil Forcada <gforcada@gnome.org>
2016-05-10 22:59:04 +0200

    Tests

---
+++ src/mr.roboto/src/mr/roboto/tests/test_changelog_entry.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+from mr.roboto.events import NewPullRequest
+from mr.roboto.subscriber import warn_if_no_changelog_entry
+from testfixtures import LogCapture
+
+import copy
+import mock
+import unittest
+
+
+"""
+Reduced set of data sent by Github about a pull request.
+
+Each payload is adapted later on to test all corner cases.
+"""
+PAYLOAD = {
+    'number': '34',
+    'html_url': 'https://github.com/plone/mr.roboto/pull/34',
+    'diff_url': 'https://github.com/plone/mr.roboto/pull/34.diff',
+    'base': {
+        'repo': {
+            'name': 'mr.roboto',
+            'owner': {
+                'login': 'plone',
+            },
+        },
+    },
+}
+
+WHITELISTED_REPO_PAYLOAD = copy.deepcopy(PAYLOAD)
+WHITELISTED_REPO_PAYLOAD['base']['repo']['name'] = 'documentation'
+
+DIFF_NO_CHANGELOG = """
+diff --git a/src/mr.roboto/setup.py b/src/mr.roboto/setup.py
+index 2a20bdc..57ce05f 100644
+--- a/src/mr.roboto/setup.py
++++ b/src/mr.roboto/setup.py
+@@ -16,6 +16,7 @@
+     'pyramid_debugtoolbar',
+     'pytest',
+     'WebTest',
++    'testfixtures',
+ ]
+
+ setup(
+"""
+
+DIFF_WITH_CHANGELOG = """
+diff --git a/src/mr.roboto/CHANGES.rst b/src/mr.roboto/CHANGES.rst
+index 2a20bdc..57ce05f 100644
+--- a/src/mr.roboto/CHANGES.rst
++++ b/src/mr.roboto/CHANGES.rst
+@@ -16,6 +16,7 @@
+     'pyramid_debugtoolbar',
+     'pytest',
+     'WebTest',
++    'testfixtures',
+ ]
+
+ setup(
+"""
+
+
+class MockRequest(object):
+
+    @property
+    def registry(self):
+        return self
+
+    @property
+    def settings(self):
+        return {
+            'github': mock.MagicMock(),
+            'roboto_url': 'http://jenkins.plone.org/roboto'
+        }
+
+
+class MockDiff(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def encoding(self):
+        return 'utf-8'
+
+    def __iter__(self):
+        for line in self.data.splitlines(True):
+            yield line
+
+
+class ChangeLogEntrySubscriberTest(unittest.TestCase):
+
+    def test_repo_whitelisted(self):
+        event = NewPullRequest(
+            pull_request=WHITELISTED_REPO_PAYLOAD,
+            request=MockRequest(),
+        )
+
+        with LogCapture() as captured_data:
+            warn_if_no_changelog_entry(event)
+
+        self.assertIn(
+            'whitelisted for changelog entries',
+            captured_data.records[-1].msg
+        )
+
+    @mock.patch('requests.get')
+    def test_no_change_log_file(self, m1):
+        m1.return_value = MockDiff(DIFF_NO_CHANGELOG)
+
+        event = NewPullRequest(
+            pull_request=PAYLOAD,
+            request=MockRequest(),
+        )
+
+        with LogCapture() as captured_data:
+            warn_if_no_changelog_entry(event)
+
+        self.assertIn(
+            'changelog entry: error',
+            captured_data.records[-1].msg
+        )
+
+    @mock.patch('requests.get')
+    def test_with_change_log_file(self, m1):
+        m1.return_value = MockDiff(DIFF_WITH_CHANGELOG)
+
+        event = NewPullRequest(
+            pull_request=PAYLOAD,
+            request=MockRequest(),
+        )
+
+        with LogCapture() as captured_data:
+            warn_if_no_changelog_entry(event)
+
+        self.assertIn(
+            'changelog entry: success',
+            captured_data.records[-1].msg
+        )
