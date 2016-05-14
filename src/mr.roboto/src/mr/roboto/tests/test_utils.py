148dbbcb24ebd6cd12706b68df055fc8e5f427fb
Gil Forcada <gforcada@gnome.org>
2016-05-14 22:57:22 +0200

    Shorten pull request URLs

---
+++ src/mr.roboto/src/mr/roboto/tests/test_utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from mr.roboto.utils import shorten_pull_request_url
+
+import unittest
+
+
+class TestShortenPRUrls(unittest.TestCase):
+
+    def test_shorten_url(self):
+        url = 'https://github.com/plone/plone.app.registry/pull/20'
+        self.assertEqual(
+            shorten_pull_request_url(url),
+            'plone.app.registry#20'
+        )
+
+    def test_fallback(self):
+        url = 'https://github.com/plone/random/url'
+        self.assertEqual(
+            shorten_pull_request_url(url),
+            url
+        )
--- src/mr.roboto/src/mr/roboto/utils.py
+++ src/mr.roboto/src/mr/roboto/utils.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import pickle
+import re
+
+
+PULL_REQUEST_URL_RE = re.compile(r'https://github.com/.+/(.+)/pull/(\d+)')
 
 
 def get_pickled_data(filename):
@@ -12,3 +16,17 @@ def plone_versions_targeted(repo, branch, request):
         return sources[(repo, branch)]
     except KeyError:
         return []
+
+
+def shorten_pull_request_url(url):
+    """Turn https://github.com/plone/plone.app.registry/pull/20 into
+    plone.app.registry#20
+
+    This way log messages can be easier to parse visually.
+
+    Fallback to returning the full URL if the shortening does not work.
+    """
+    re_result = PULL_REQUEST_URL_RE.match(url)
+    if re_result:
+        return '{0}#{1}'.format(*re_result.groups())
+    return url
