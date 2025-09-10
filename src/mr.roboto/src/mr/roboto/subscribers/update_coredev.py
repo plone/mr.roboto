accca9decc139177a0af82fc6afef3f0ffd86ec8
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-09-10 14:54:13 +0200

    chore: split method into smaller ones
    Readability counts :-)

--- src/mr.roboto/src/mr/roboto/subscribers/update_coredev.py
+++ src/mr.roboto/src/mr/roboto/subscribers/update_coredev.py
@@ -10,6 +10,7 @@ from mr.roboto.utils import plone_versions_targeted
 from pyramid.events import subscriber
 
 
+# Changes in these repositories should not update buildout.coredev files
 IGNORE_NO_AUTO_CHECKOUT = (
     "documentation",
     "icalendar",
@@ -32,54 +33,75 @@ class UpdateCoredevCheckouts(PullRequestSubscriber):
         - on all Plone coredev versions that track the branch that was
         targeted by the pull request
         """
-        # pull requests on buildout.coredev itself do not need any extra work
-        if self.repo_full_name == "plone/buildout.coredev":
+        if not self.needs_update():
             return
 
-        if self.repo_name in IGNORE_NO_AUTO_CHECKOUT:
+        plone_versions = self.plone_versions()
+        if not plone_versions:
             return
+
+        self.add_package_to_checkouts(plone_versions)
+
+    def needs_update(self):
+        """Check if we really need to update buildout.coredev"""
+        # pull requests on buildout.coredev itself do not need any extra work
+        if self.repo_full_name == "plone/buildout.coredev":
+            return False
+
+        if self.repo_name in IGNORE_NO_AUTO_CHECKOUT:
+            return False
 
         if self.pull_request_author in IGNORE_PR_AUTHORS:
             self.log(
                 f"no commits on buildout.coredev as "
                 f"user {self.pull_request_author} is ignored"
             )
-            return
+            return False
 
         if self.is_skip_commit():
             self.log("Commit had a skip CI mark. No commit is done in buildout.coredev")
-            return
+            return False
 
-        plone_versions = plone_versions_targeted(
-            self.repo_full_name, self.target_branch, self.event.request
-        )
-        if not plone_versions:
-            self.log(
-                f"no plone coredev version tracks branch {self.target_branch} "
-                f"of {self.repo_name}, checkouts.cfg not updated"
-            )
-            return
-
-        checkouts = get_pickled_data(
-            self.event.request.registry.settings["checkouts_file"]
-        )
-        not_in_checkouts = [
-            version
-            for version in plone_versions
-            if self.repo_name not in checkouts[version]
-        ]
-        if not not_in_checkouts:
-            self.log(
-                f"is already on checkouts.cfg of all plone "
-                f"versions that it targets {plone_versions}"
-            )
-            return
-
-        self.add_package_to_checkouts(not_in_checkouts)
+        return True
 
     def is_skip_commit(self):
         commit_message = self.g_merge_commit.commit.message
         return is_skip_commit_message(commit_message)
+
+    def plone_versions(self):
+        versions = plone_versions_targeted(
+            self.repo_full_name, self.target_branch, self.event.request
+        )
+        if not versions:
+            self.log(
+                f"no plone coredev version tracks branch {self.target_branch} "
+                f"of {self.repo_name}, checkouts.cfg not updated"
+            )
+            return []
+
+        versions = self._already_in_all_checkouts(versions)
+        return versions
+
+    def _already_in_all_checkouts(self, plone_versions):
+        """Which plone versions are missing the current repository in its checkouts?
+
+        Given the list of plone versions the current repository is targeting,
+        in which of those plone versions is the current repository not in checkouts.cfg?
+        """
+        checkouts = get_pickled_data(
+            self.event.request.registry.settings["checkouts_file"]
+        )
+        plone_versions_missing = [
+            version
+            for version in plone_versions
+            if self.repo_name not in checkouts[version]
+        ]
+        if not plone_versions_missing:
+            self.log(
+                f"is already on checkouts.cfg of all plone "
+                f"versions that it targets {plone_versions}"
+            )
+        return plone_versions_missing
 
     def add_package_to_checkouts(self, versions):
         """Add package to checkouts.cfg on buildout.coredev plone version"""
